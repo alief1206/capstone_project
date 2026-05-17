c747f397350a5a346f141225331691c22bb95bc0
alief1206 <fqturalief15@gmail.com>
2026-05-17 22:17:07 +0700

    back-end sementara

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import foodLogRoutes from './routes/foodLogRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/food-logs', foodLogRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Endpoint RESTful API tidak ditemukan!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Back-End aktif dan berjalan aman di port ${PORT}`);
+});
